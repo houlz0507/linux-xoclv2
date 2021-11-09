baf2394afa41841d2c6fe5b43fbbee304fc7120e
Lizhi Hou <lizhi.hou@xilinx.com>
2021-11-09 18:51:47 -0500

    fpga: xrt: management physical function driver
    The PCIE device driver which attaches to management function on Alveo
    devices. It instantiates one or more group drivers which, in turn,
    instantiate xrt drivers. The instantiation of group and xrt drivers is
    completely dtb driven. A test dtb is used for this patchset.
    
    Signed-off-by: Sonal Santan <sonal.santan@xilinx.com>
    Signed-off-by: Max Zhen <max.zhen@xilinx.com>
    Signed-off-by: Lizhi Hou <lizhi.hou@xilinx.com>

---
+++ drivers/fpga/xrt/mgmt/dt-test.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2020-2021 Xilinx, Inc.
+ *
+ * Authors:
+ *	Lizhi Hou <lizhih@xilinx.com>
+ */
+
+#ifndef _DT_TEST_H_
+#define _DT_TEST_H_
+
+extern u8 __dtb_dt_test_begin[];
+extern u8 __dtb_dt_test_end[];
+
+#endif	/* _DT_TEST_H_ */
